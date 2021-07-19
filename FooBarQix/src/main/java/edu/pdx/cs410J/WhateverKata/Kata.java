bae03f22c5a8e7b91caa893d4ebb1b8f418a4176
Minh Le <minh26@pdx.edu>
2021-07-18 23:24:03 -0700

    Finish

--- FooBarQix/src/main/java/edu/pdx/cs410J/WhateverKata/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/WhateverKata/Kata.java
@@ -1,35 +1,71 @@
 package edu.pdx.cs410J.WhateverKata;
 
+import java.text.ParseException;
+
 /**
  * A class for getting started with a code kata
- *
+ * <p>
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
 
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
-  }
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Missing command line arguments");
+            System.exit(1);
+        } else if (args.length > 1) {
+            System.err.println("Too many arguments");
+            System.exit(1);
+        }
 
-  public String transformString( String s ) {
-    String result = "";
-
-    for( int i = 0; i < s.length(); ++i ){
-      switch(s.charAt(i)){
-        case '3':
-          result = result + "Foo";
-          break;
-        default:
-          result = result + s.charAt(i);
-          break;
-      }
+        System.out.println(transformString(args[0]));
     }
 
-    System.out.println(result);
-      return result;
-  }
+    public static String transformString(String s) {
+        boolean check = false;
+        String result = "";
+
+        try {
+            int number = Integer.parseInt(s);
+            if (number % 3 == 0){
+                result += "Foo";
+                check = true;
+            }
+            if (number % 5 == 0){
+                result += "Bar";
+                check = true;
+            }
+            if (number % 7 == 0){
+                result += "Qix";
+                check = true;
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("Wrong format");
+            System.exit(1);
+        }
+
+        for (int i = 0; i < s.length(); ++i) {
+            switch (s.charAt(i)) {
+                case '0':
+                    result += '*';
+                    break;
+                case '3':
+                    result += "Foo";
+                    break;
+                case '5':
+                    result += "Bar";
+                    break;
+                case '7':
+                    result += "Qix";
+                    break;
+                default:
+                    if(!check)
+                        result += s.charAt(i);
+                    break;
+            }
+        }
+        return result;
+    }
 }
 
--- FooBarQix/src/test/java/edu/pdx/cs410J/WhateverKata/KataTest.java
+++ FooBarQix/src/test/java/edu/pdx/cs410J/WhateverKata/KataTest.java
@@ -1,34 +1,64 @@
 package edu.pdx.cs410J.WhateverKata;
 
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class KataTest
-{
+public class KataTest extends InvokeMainTestCase {
 
-  @Test
-  void canInstantiateKataClass() {
-    new Kata();
-  }
+    @Test
+    void canInstantiateKataClass() {
+        new Kata();
+    }
 
-  @Test
-  void canWorkWithNumber1() {
-    Kata kata = new Kata();
+    private InvokeMainTestCase.MainMethodResult invokeMain(String... args) {
+        return invokeMain(Kata.class, args);
+    }
 
-    String result = kata.transformString( "1" );
+    @Test
+    void testNoCommandLineArguments() {
+        InvokeMainTestCase.MainMethodResult result = invokeMain();
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+        assertThat(result.getExitCode(), CoreMatchers.equalTo(1));
+    }
 
-    assertThat( result, equalTo("1") );
-  }
+    @Test
+    void moreThanOneArguments() {
+        InvokeMainTestCase.MainMethodResult result = invokeMain("124", "234", "2345");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Too many arguments"));
+        assertThat(result.getExitCode(), CoreMatchers.equalTo(1));
+    }
 
-  @Test
-  void canWorkWithNumber3() {
-    Kata kata = new Kata();
+    @Test
+    void wrongFormat() {
+        InvokeMainTestCase.MainMethodResult result = invokeMain("adsf");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Wrong format"));
+        assertThat(result.getExitCode(), CoreMatchers.equalTo(1));
+    }
 
-    String result = kata.transformString( "3" );
 
-    assertTrue( result == "Foo" );
-  }
+    @Test
+    void canWorkWithNumber1() {
+        Kata kata = new Kata();
+
+        String result = kata.transformString("1");
+
+        assertThat(result, equalTo("1"));
+    }
+
+    @Test
+    void canWorkWithNumber3() {
+        Kata kata = new Kata();
+
+        String result = kata.transformString("3");
+
+        assertTrue(result.equals("FooFoo"));
+    }
+
+
 }
